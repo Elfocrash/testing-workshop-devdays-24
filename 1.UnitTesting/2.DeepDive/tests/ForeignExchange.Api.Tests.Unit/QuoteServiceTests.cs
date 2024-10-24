03c9c6e6c79353ddd8fbf411d6aa67f29c118626
Nick Chapsas <nick.chapsas@gmail.com>
2024-10-24 11:56:16 +0200

    pre ex

--- 1.UnitTesting/2.DeepDive/tests/ForeignExchange.Api.Tests.Unit/QuoteServiceTests.cs
+++ 1.UnitTesting/2.DeepDive/tests/ForeignExchange.Api.Tests.Unit/QuoteServiceTests.cs
@@ -1,14 +1,11 @@
 using FluentAssertions;
-using ForeignExchange.Api.Database;
 using ForeignExchange.Api.Logging;
 using ForeignExchange.Api.Models;
 using ForeignExchange.Api.Repositories;
 using ForeignExchange.Api.Services;
 using ForeignExchange.Api.Validation;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
-using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReturnsExtensions;
 using Xunit;
 
 namespace ForeignExchange.Api.Tests.Unit;
@@ -53,9 +50,9 @@ public class QuoteServiceTests
 
         // Act
         var result = await _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
-
+        
         // Assert
-        result!.Should().BeEquivalentTo(expectedQuote);
+        result.Should().BeEquivalentTo(expectedQuote);
     }
 
     [Fact]
@@ -100,5 +97,38 @@ public class QuoteServiceTests
             Arg.Is<object?[]>(x => 
                 x[0]!.ToString() == fromCurrency &&
                 x[1]!.ToString() == toCurrency));
+    }
+    
+    [Fact]
+    public async Task GetQuoteAsync_ShouldThrowException_WhenSameCurrencyIsUsed()
+    {
+        // Arrange
+        var fromCurrency = "USD";
+        var toCurrency = "USD";
+        var amount = 100;
+        
+        // Act
+        var action = () => _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
+
+        // Assert
+        await action.Should().ThrowAsync<SameCurrencyException>()
+            .WithMessage($"You cannot convert currency {fromCurrency} to itself");
+    }
+    
+    [Fact]
+    public async Task GetQuoteAsync_ShouldReturnNull_WhenNoRateExists()
+    {
+        // Arrange
+        var fromCurrency = "USD";
+        var toCurrency = "GBP";
+        var amount = 100;
+
+        _ratesRepository.GetRateAsync(fromCurrency, toCurrency).ReturnsNull();
+
+        // Act
+        var result = await _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
+
+        // Assert
+        result.Should().BeNull();
     }
 }
